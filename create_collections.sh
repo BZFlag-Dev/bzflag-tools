caf1a4590da82da8ee55329078b3f6cbc89233cc
Jeff Makey <jeff@makey.net>
2014-11-11 07:54:18 -0800

    Create 3 collection repos consisting of the 13 non-game repos and svn_to_git. Temporarily disable use of svn2git.

--- create_collections.sh
+++ create_collections.sh
@@ -10,6 +10,24 @@
 # IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
+(
+date "+svn_to_git started %c"
+repo=/tmp/svn2git.svn_to_git
+exec > $repo.log 2>&1
+set -xe
+rm -rf $repo
+git clone file:///$HOME/bzflag/git-master $repo
+cd $repo
+git remote remove origin
+git filter-branch --subdirectory-filter svn_to_git master | tr \\r \\n
+rm -rf .git/refs/original	# discard old commits saved by filter-branch
+git gc --prune=now				# tidy
+rm -f .git/FETCH_HEAD				# tidy
+rm -r .git/logs/refs/remotes .git/refs/remotes	# tidy
+git status --ignored				# update index and show state
+)
+
+# if false ; then
 for repo in admin bzauthd bzedit bzeditw32 bzstats bzview bzwgen bzworkbench custom_plugins db pybzflag tools web bzflag ; do
 	date "+$repo started %c"
 	svn2git.sh $repo
@@ -18,4 +36,56 @@ for repo in admin bzauthd bzedit bzeditw32 bzstats bzview bzwgen bzworkbench cus
 		echo $repo: status $status
 	fi
 done
+# fi
 date
+
+combine_repos()
+{
+combined_repo=/tmp/svn2git.$1
+shift
+rm -rf $combined_repo
+git init $combined_repo
+cd $combined_repo
+for svn_repo in $* ; do
+	git remote add -f temp /tmp/svn2git.$svn_repo
+	case $svn_repo in
+	    admin)
+		repo=masterban
+		;;
+	    custom_plugins)
+		repo=irclink_plugin
+		;;
+	    tools)
+		repo=stat_collector
+		;;
+	    *)
+		repo=$svn_repo
+		;;
+	esac
+	for branch in `git branch -a` ; do
+		case $branch in
+		    remotes/temp/master)
+			git branch $repo $branch
+			;;
+		    remotes/temp/*)
+			local=`echo $branch | sed 's=^remotes/temp/=='`
+			git branch ${repo}_$local $branch
+			;;
+		esac
+	done
+	git remote remove temp
+	# rm -rf /tmp/svn2git.$svn_repo
+done
+git checkout $repo
+git gc --prune=now				# tidy
+rm -f .git/FETCH_HEAD				# tidy
+rm -r .git/logs/refs/remotes .git/refs/remotes	# tidy
+git status --ignored				# update index and show state
+}
+
+(
+set -xe
+combine_repos bzflag-archive pybzflag custom_plugins
+combine_repos bzflag-tools bzedit bzview bzeditw32 bzworkbench tools bzwgen svn_to_git
+combine_repos bzflag-web bzstats bzauthd admin db web
+) > /tmp/svn2git.combine_repos.log 2>&1
