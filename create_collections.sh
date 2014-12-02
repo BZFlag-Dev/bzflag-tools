1d1bec11777d2c2da252e269bd079c6866150d7d
Jeff Makey <jeff@makey.net>
2014-12-01 21:41:20 -0800

    Create a bzflag collection repo that includes bzauthd (moved from web). Conjoin the 2.99_bzauthd and bzauthd branches. Use the UPSTREAM_UUID variable rather than its hard coded value.

--- create_collections.sh
+++ create_collections.sh
@@ -56,7 +56,7 @@ rm -rf $combined_repo
 git init $combined_repo
 cd $combined_repo
 for svn_repo_name in $* ; do
-	git remote add -f temp $BASE/svn2git.$svn_repo_name
+	git remote add --tags -f temp $BASE/svn2git.$svn_repo_name
 	case $svn_repo_name in
 	    admin)
 		repo=masterban
@@ -72,6 +72,7 @@ for svn_repo_name in $* ; do
 		;;
 	esac
 	for branch in `git branch -a` ; do
+		# preserve master branch name in bzflag repo here
 		case $branch in
 		    remotes/temp/master)
 			git branch $repo $branch
@@ -83,10 +84,74 @@ for svn_repo_name in $* ; do
 		esac
 	done
 	git remote remove temp
-	if [ $svn_repo_name = web ] ; then		# admin and db repos are already done
+	if [ $svn_repo_name = bzflag ] ; then		# bzauthd repo is already done
+		# conjoin the 2.99_bzauthd and bzauthd branches
+		git checkout :/@18217.$UPSTREAM_UUID
+		git merge --no-commit -Xours :/@18154.$UPSTREAM_UUID
+		git mv bzauthd ldap libgcrypt libgpg-error tcp-net src
+		git mv test src/bzauthd_test
+		git rm -q -f -r MSVC/VC7.1
+		LOCATION=branches/gsoc_bzauthd
+		rev=18218
+		DATE="`svn log --xml -r $rev $SVN_REPO | perl -wle 'undef \$/; \$_ = <>; s=.*<date>==s and s=</date>.*==s and print'`"
+		AUTHOR="`svn log --xml -r $rev $SVN_REPO | perl -wle 'undef \$/; \$_ = <>; s=.*<author>==s and s=</author>.*==s and print'`"
+		MESSAGE="`svn log --xml -r $rev $SVN_REPO | perl -wle 'undef \$/; \$_ = <>; s=.*<msg>==s and s=</msg>.*==s and print'`"
+		git commit --allow-empty "--date=$DATE" "--author=$AUTHOR" "-m$MESSAGE
+
+git-svn-id: $UPSTREAM_REPO/$LOCATION@$rev $UPSTREAM_UUID"
+		git cherry-pick --allow-empty :/@18218.$UPSTREAM_UUID..:/@18415.$UPSTREAM_UUID
+
+		# synthesize r19840
+		git merge --no-commit -Xtheirs :/@19834.$UPSTREAM_UUID
+		git rm -q -r MSVC/VC8
+		git rm -q -f src/ogl/OpenGLContext.cxx src/other/freetype/builds/win32/visualc/freetype_vc8.vcproj src/other/freetype/include/freetype/ftcid.h src/other/freetype/include/freetype/internal/services/svcid.h src/other/freetype/include/freetype/internal/services/svttglyf.h
+		rev=19840
+		EXCEPTIONS='MSVC/build/bzflag.sln MSVC/build/bzflag.vcproj MSVC/build/bzfs.sln include/ServerList.h plugins/configure.ac src/bzflag/bzflag.cxx src/bzfs/ListServerConnection.cxx src/bzfs/bzfs.cxx src/common/global.cxx src/game/ServerList.cxx src/other/curl/buildconf.bat'
+		SUBDIR=
+		for file in $EXCEPTIONS ; do
+			svn cat $SVN_REPO/$LOCATION/$file@$rev > $SUBDIR$file
+			case $file in
+			    configure.ac|plugins/HoldTheFlag/HoldTheFlag.cpp|plugins/nagware/nagware.cpp|src/bzflag/ScoreboardRenderer.cxx)
+				sed -i -e 's/\$Id: .* \$/$Id$/' -e 's/\$Revision: .* \$/$Revision$/' $SUBDIR$file	# unexpand keywords
+				;;
+			esac
+			git add $SUBDIR$file
+		done
+		DATE="`svn log --xml -r $rev $SVN_REPO | perl -wle 'undef \$/; \$_ = <>; s=.*<date>==s and s=</date>.*==s and print'`"
+		AUTHOR="`svn log --xml -r $rev $SVN_REPO | perl -wle 'undef \$/; \$_ = <>; s=.*<author>==s and s=</author>.*==s and print'`"
+		MESSAGE="`svn log --xml -r $rev $SVN_REPO | perl -wle 'undef \$/; \$_ = <>; s=.*<msg>==s and s=</msg>.*==s and print'`"
+		git commit --allow-empty "--date=$DATE" "--author=$AUTHOR" "-m$MESSAGE
+
+git-svn-id: $UPSTREAM_REPO/$LOCATION@$rev $UPSTREAM_UUID"
+
+		# synthesize r19841
+		git merge --no-commit :/@19838.$UPSTREAM_UUID
+		rev=19841
+		EXCEPTIONS=src/bzfs/bzfs.cxx
+		for file in $EXCEPTIONS ; do
+			svn cat $SVN_REPO/$LOCATION/$file@$rev > $SUBDIR$file
+			case $file in
+			    configure.ac|plugins/HoldTheFlag/HoldTheFlag.cpp|plugins/nagware/nagware.cpp|src/bzflag/ScoreboardRenderer.cxx)
+				sed -i -e 's/\$Id: .* \$/$Id$/' -e 's/\$Revision: .* \$/$Revision$/' $SUBDIR$file	# unexpand keywords
+				;;
+			esac
+			git add $SUBDIR$file
+		done
+		DATE="`svn log --xml -r $rev $SVN_REPO | perl -wle 'undef \$/; \$_ = <>; s=.*<date>==s and s=</date>.*==s and print'`"
+		AUTHOR="`svn log --xml -r $rev $SVN_REPO | perl -wle 'undef \$/; \$_ = <>; s=.*<author>==s and s=</author>.*==s and print'`"
+		MESSAGE="`svn log --xml -r $rev $SVN_REPO | perl -wle 'undef \$/; \$_ = <>; s=.*<msg>==s and s=</msg>.*==s and print'`"
+		git commit --allow-empty "--date=$DATE" "--author=$AUTHOR" "-m$MESSAGE
+
+git-svn-id: $UPSTREAM_REPO/$LOCATION@$rev $UPSTREAM_UUID"
+		git branch new_bzauthd	# an easy way to mark the current location
+		git rebase --keep-empty --preserve-merges -Xours new_bzauthd 2.99_bzauthd | tr \\r \\n
+		git branch -d new_bzauthd
+		git filter-branch --env-filter 'export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME";export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL";export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"' -- --all | tr \\r \\n
+		rm -rf .git/refs/original	# discard old commits saved by filter-branch
+	elif [ $svn_repo_name = web ] ; then		# admin and db repos are already done
 		# conjoin the admin and web branches
-		git checkout :/@8126.08b3d480
-		git merge --no-commit -Xours :/@8149.08b3d480
+		git checkout :/@8126.$UPSTREAM_UUID
+		git merge --no-commit -Xours :/@8149.$UPSTREAM_UUID
 		git rm -q -f -r .cvsignore [^m]* master_ban.txt
 		rev=8157
 		for file in master-bans.txt ; do
@@ -104,8 +169,8 @@ git-svn-id: $UPSTREAM_REPO/trunk/admin@$rev $UPSTREAM_UUID"
 		git branch -d new_masterban
 
 		# merge the db branch into the web branch
-		git checkout :/@22223.08b3d480
-		git merge --no-commit :/@22064.08b3d480
+		git checkout :/@22223.$UPSTREAM_UUID
+		git merge --no-commit :/@22064.$UPSTREAM_UUID
 		mkdir bzfls bzfls/css bzfls/images oldstats
 		git mv css/weblogin.css bzfls/css
 		git mv images/webauth_*.png bzfls/images
@@ -125,21 +190,21 @@ git-svn-id: $UPSTREAM_REPO/trunk/admin@$rev $UPSTREAM_UUID"
 
 git-svn-id: $UPSTREAM_REPO/trunk/db@$rev $UPSTREAM_UUID"
 		git branch new_web	# an easy way to mark the current location
-		git rebase --keep-empty -Xours new_web :/@22369.08b3d480 | tr \\r \\n
-		git cherry-pick --allow-empty :/@22427.08b3d480		# merge fails to keep it
+		git rebase --keep-empty -Xours new_web :/@22369.$UPSTREAM_UUID | tr \\r \\n
+		git cherry-pick --allow-empty :/@22427.$UPSTREAM_UUID	# merge fails to keep it
 		git rev-parse HEAD > .git/refs/heads/new_web
 		git rebase --keep-empty -Xours new_web web | tr \\r \\n	# merge the remaining web branch
 		git branch -d new_web
 		git branch -m gamestats_live old_gamestats_live
-		git checkout -b gamestats_live :/@22436.08b3d480
-		if ! git cherry-pick :/@22437.08b3d480 ; then
+		git checkout -b gamestats_live :/@22436.$UPSTREAM_UUID
+		if ! git cherry-pick :/@22437.$UPSTREAM_UUID ; then
 			git rm -q -r bzfls
 			sed -i '1,/^git-svn-id:/!d' .git/MERGE_MSG
 			git commit --allow-empty -F .git/MERGE_MSG	# instead of "git cherry-pick --continue"
 		fi
 		LOCATION=branches/gamestats_live
 		rev=22442
-		git merge -q --no-commit -Xsubtree=gamestats :/@22441.08b3d480
+		git merge -q --no-commit -Xsubtree=gamestats :/@22441.$UPSTREAM_UUID
 		for file in config/config.php ; do
 			svn cat $SVN_REPO/$LOCATION/$file@$rev > $file
 			git add $file
@@ -151,7 +216,7 @@ git-svn-id: $UPSTREAM_REPO/trunk/db@$rev $UPSTREAM_UUID"
 
 git-svn-id: $UPSTREAM_REPO/$LOCATION@$rev $UPSTREAM_UUID"
 		rev=22471
-		git merge -q --no-commit -Xsubtree=gamestats :/@22470.08b3d480
+		git merge -q --no-commit -Xsubtree=gamestats :/@22470.$UPSTREAM_UUID
 		DATE="`svn log --xml -r $rev $SVN_REPO | perl -wle 'undef \$/; \$_ = <>; s=.*<date>==s and s=</date>.*==s and print'`"
 		AUTHOR="`svn log --xml -r $rev $SVN_REPO | perl -wle 'undef \$/; \$_ = <>; s=.*<author>==s and s=</author>.*==s and print'`"
 		MESSAGE="`svn log --xml -r $rev $SVN_REPO | perl -wle 'undef \$/; \$_ = <>; s=.*<msg>==s and s=</msg>.*==s and print'`"
@@ -175,5 +240,6 @@ git status --ignored				# update index and show state
 set -xe
 combine_repos bzflag-archive pybzflag custom_plugins
 combine_repos bzflag-tools bzedit bzview bzeditw32 bzstats bzworkbench tools bzwgen svn_to_git
-combine_repos bzflag-web bzauthd admin db web
+combine_repos bzflag-web admin db web
+combine_repos bzflag-bzflag bzauthd bzflag
 ) > $BASE/svn2git.combine_repos.log 2>&1
