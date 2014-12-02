502b35296e3aefbde739deb97278942b138b6e49
Jeff Makey <jeff@makey.net>
2014-12-02 10:48:24 -0800

    Make the filter-branch filters more readable.

--- svn2git.sh
+++ svn2git.sh
@@ -523,11 +523,32 @@ done < $SOURCE/revision_list
 IFS="$SAVEIFS"
 
 # change all committer info to match the author's
+COMMITTER_IS_AUTHOR='
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"'
+
 # move files up out of repo name subdirectory
 # move files up out of BZStatCollector and irclink subdirectories
 # (the file name bzFLAG is known not to conflict with anything)
 # "--subdirectory-filter bzflag" removes empty commits and so is unsuitable
-time git filter-branch --env-filter 'export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME";export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL";export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"' --tree-filter "if mv $TARGET_REPO bzFLAG ; then mv bzFLAG/.??* bzFLAG/* . || true ; rmdir bzFLAG ; if test -d BZStatCollector ; then mv BZStatCollector/* . || true ; rmdir BZStatCollector ; elif test -d irclink ; then mv irclink/* . || true ; rmdir irclink ; fi ; fi" --msg-filter 'perl -0 -wpe s/CVS:.\*//g\;s/\\n\*\(git-svn-id:\)/\\n\\n\$1/' -- --all | tr \\r \\n
+TREE_FILTER="
+if mv $TARGET_REPO bzFLAG ; then
+ mv bzFLAG/.??* bzFLAG/* . || true
+ rmdir bzFLAG
+ if test -d BZStatCollector ; then
+  mv BZStatCollector/* . || true
+  rmdir BZStatCollector
+ elif test -d irclink ; then
+  mv irclink/* . || true
+  rmdir irclink
+ fi
+fi"
+
+# remove bogus CVS: text
+MSG_FILTER='perl -0 -wpe s/CVS:.\*//g\;s/\\n\*\(git-svn-id:\)/\\n\\n\$1/'
+
+time git filter-branch --env-filter "$COMMITTER_IS_AUTHOR" --tree-filter "$TREE_FILTER" --msg-filter "$MSG_FILTER" -- --all | tr \\r \\n
 rm -rf .git/refs/original	# discard old commits saved by filter-branch
 
 if [ $TARGET_REPO = db -a $NEXT_REVISION -gt 22835 ] ; then
@@ -537,7 +558,7 @@ if [ $TARGET_REPO = db -a $NEXT_REVISION -gt 22835 ] ; then
 	fi
 	git branch bzauthd_db	# branch is now properly attached
 	git branch -d -r gsoc_bzauthd_db
-	git filter-branch --env-filter 'export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME";export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL";export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"' -- :/@20270.08b3d480..bzauthd_db | tr \\r \\n
+	git filter-branch --env-filter "$COMMITTER_IS_AUTHOR" -- :/@20270.08b3d480..bzauthd_db | tr \\r \\n
 	rm -rf .git/refs/original	# discard old commits saved by filter-branch
 fi
 
@@ -623,8 +644,16 @@ EOF
 	git branch -d -r ftgl gsoc_08_libbzw gsoc_server_listing remove_flag_id tags/merge-2_0-2_1-1 tags/merge-2_0-2_1-2 tags/merge-2_0-2_1-3 tags/merge-2_0-2_1-4 tags/merge-2_0-2_1-5 tags/merge-2_0-2_1-6 tags/merge-2_0-2_1-7 tags/merge-2_0-2_1-8 tags/merge-2_0-2_1-9 tags/pre-mesh tags/soc-irc tags/v1_11_12 tags/v1_11_14 tags/v1_11_16 tags/v1_7d_6 tags/v1_7d_7 tags/v1_7d_8 tags/v1_7d_9 tags/v1_7temp tags/v1_8abort tags/v1_9_4_Beta tags/v1_9_6_Beta tags/v1_9_7_Beta tags/v1_9_8_Beta tags/v1_9_9_Beta tags/v2_0_10RC3 tags/v2_0_10_RC1 tags/v2_0_10_RC2 tags/v2_0_12.deleted tags/v2_0_4_rc1 tags/v2_0_4_rc4 tags/v2_0_4_rc5 tags/v2_99archive tags/v3_0_alpha1 tags/v3_0_alpha2 || true
 
 	# fix some e-mail addresses and full names
+	CANONICAL_AUTHORS='
+test $GIT_AUTHOR_EMAIL = josh@savannah.local && export GIT_AUTHOR_EMAIL=josh@joshb.us
+test $GIT_AUTHOR_EMAIL = jwmelto@Goliath.local -o $GIT_AUTHOR_EMAIL = jwmelto@comcast.net && export GIT_AUTHOR_EMAIL=jwmelto@users.sourceforge.net
+test $GIT_AUTHOR_EMAIL = jeffm2501@gmail.com && export GIT_AUTHOR_EMAIL=JeffM2501@gmail.com
+test $GIT_AUTHOR_EMAIL = kongr45gpen@helit.org && export GIT_AUTHOR_EMAIL=electrovesta@gmail.com
+test $GIT_AUTHOR_EMAIL = allejo@users.noreply.github.com && export GIT_AUTHOR_EMAIL=allejo@me.com
+test $GIT_AUTHOR_EMAIL = allejo@me.com && export GIT_AUTHOR_NAME=allejo'
+
 	# change committer info to match the author's
-	git filter-branch --env-filter 'test $GIT_AUTHOR_EMAIL = josh@savannah.local && export GIT_AUTHOR_EMAIL=josh@joshb.us;test $GIT_AUTHOR_EMAIL = jwmelto@Goliath.local -o $GIT_AUTHOR_EMAIL = jwmelto@comcast.net && export GIT_AUTHOR_EMAIL=jwmelto@users.sourceforge.net;test $GIT_AUTHOR_EMAIL = jeffm2501@gmail.com && export GIT_AUTHOR_EMAIL=JeffM2501@gmail.com;test $GIT_AUTHOR_EMAIL = kongr45gpen@helit.org && export GIT_AUTHOR_EMAIL=electrovesta@gmail.com;test $GIT_AUTHOR_EMAIL = allejo@users.noreply.github.com && export GIT_AUTHOR_EMAIL=allejo@me.com;test $GIT_AUTHOR_EMAIL = allejo@me.com && export GIT_AUTHOR_NAME=allejo;export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME";export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL";export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"' -- trunk..2.4 trunk..2.5 | tr \\r \\n
+	git filter-branch --env-filter "$CANONICAL_AUTHORS$COMMITTER_IS_AUTHOR" -- trunk..2.4 trunk..2.5 | tr \\r \\n
 	rm -r .git/refs/original	# discard old commits saved by filter-branch
 
 	# multiple passes are required to update commit hashes in commit messages
