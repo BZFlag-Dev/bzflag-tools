f421cc7ed3bb9f37605c9cf821fff0fd3720049d
Jeff Makey <jeff@makey.net>
2015-01-24 05:53:03 +0000

    r8130 completely removes trunk/admin. Handle more branches and tags that don't have a "bzflag" subdirectory. Improve serialization to prevent race conditions.

--- validate.sh
+++ validate.sh
@@ -34,19 +34,28 @@ for repo in $REPO_LIST ; do
 	GIT_DIR=$BASE/svn2git.$repo/.git git log --all | awk \$1==\"git-svn-id:\"\&\&\$3==\"$UPSTREAM_UUID\"\{print\ gensub\(\"@\",\"\ \",1,gensub\(\"$UPSTREAM_REPO/\",\"\",1,\$2\)\),\"$repo\"\} 
 done | sort -n -k2 | while read dir rev repo ; do
 	if [ $rev -gt $lastrev ] ; then
+		case $lastrev in
+		    8130)
+			wait				# serialize
+			cd $BASE			# be somewhere else
+			rm -r $GITDIR/trunk/admin/.git
+			rmdir $GITDIR/trunk/admin	# should be empty
+			;;
+		esac
 		if [ $rev -ge $STARTING_REVISION ] ; then
 			cd $SVNDIR
 			svn revert -q -R .
-			svn up -q -r $lastrev
+			svn update -q -r $lastrev
+			# undo keyword expansion, which Git does not support
 			svn propdel -q -R svn:keywords .
-			wait	# serialize
 			svn diff --ignore-properties | patch -s -p0 -R
+			wait	# serialize before reading the Git tree
 			diff -r -x .git -x .svn $GITDIR $SVNDIR
 		fi
 		lastrev=$rev
 	fi
 	case $dir in
-	    branches|branches/gamestats_live|branches/gsoc_bzauthd_db|trunk*)
+	    branches|branches/experimental/2_4_OSX_Lion_Rebuild_branch|branches/experimental/v2_99_*_branch|branches/gamestats_live|branches/gsoc_bzauthd_db|branches/summer_of_code/gsoc_[^i]*|tags/soc-bz*|tags/v2_0_10_RC[23]|tags/v2_0_1[246]|tags/v2_4_?|tags/v3_*|trunk*)
 		realdir=$dir
 		;;
 	    *)
@@ -56,6 +65,7 @@ done | sort -n -k2 | while read dir rev repo ; do
 #	echo $rev $realdir $repo
 	echo -n "$rev "
 	cd $GITDIR
+	wait	# serialize before modifiying the Git tree
 	case $rev in
 	    1)
 		mkdir branches tags trunk
@@ -69,7 +79,6 @@ done | sort -n -k2 | while read dir rev repo ; do
 		cd $realdir
 		git clone -q --shared $BASE/svn2git.$repo .
 	fi
-	wait	# serialize
 	git checkout -q :/$dir@$rev.$UPSTREAM_UUID &	# parallelize
 done
 echo ''
