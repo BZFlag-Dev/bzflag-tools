049a025d8adb83784b42283d7aa3173ccf438132
Jeff Makey <jeff@makey.net>
2015-03-18 22:42:42 -0700

    Add the beginnings of two scripts that will validate commit metadata.

---
+++ git_log_to_xml.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl -T -w
+use strict;
+# bzflag
+# Copyright (c) 1993-2015 Tim Riker
+#
+# This package is free software;  you can redistribute it and/or
+# modify it under the terms of the license found in the file
+# named COPYING that should have accompanied this file.
+#
+# THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+my ($revision, $author, $date, $msg);
+
+sub print_log_entry()
+{
+if (defined $author) {
+	$author = "<author>$author</author>\n";
+	}
+else {
+	$author = '';
+	}
+$msg =~ s/^\n//;
+chomp $msg;
+chomp $msg;
+chomp $msg;
+$msg =~ s/&/&amp;/g;
+$msg =~ s/</&lt;/g;
+$msg =~ s/>/&gt;/g;
+print "<logentry\n   revision=\"$revision\">\n$author<date>$date</date>\n<msg>$msg\n</msg>\n</logentry>\n";
+$revision = $author = $date = $msg = undef;
+}
+
+print '<?xml version="1.0" encoding="UTF-8"?>', "\n<log>\n";
+while (<>) {
+	if (/^commit [0-9a-f]{40}$/) {
+		# flush previous
+		print_log_entry if (defined $revision or defined $date or defined $msg);
+		}
+	elsif (/^Merge: [0-9a-f]{7} [0-9a-f]{7}$/) {
+		# ignore
+		}
+	elsif (/^Author: ([^<]+) <([^\@]+)\@users\.sourceforge\.net>$/) {
+		$author = $2 unless ($1 eq 'cvs2svn');
+		}
+	elsif (/^Date:   (\d\d\d\d-\d\d-\d\d) (\d\d:\d\d:\d\d) \+0000$/) {
+		$date = "$1T${2}Z";
+		}
+	elsif (m=^    git-svn-id: https://svn.code.sf.net/p/bzflag/code/[^\@]+@(\d+) 08b3d480-bf2c-0410-a26f-811ee3361c24$=) {
+		$revision = $1;
+		}
+	else {
+		s/^    //;
+		$msg .= $_;
+		}
+	}
+print_log_entry;
+print "</log>\n";
---
+++ validate_metadata.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+# bzflag
+# Copyright (c) 1993-2015 Tim Riker
+#
+# This package is free software;  you can redistribute it and/or
+# modify it under the terms of the license found in the file
+# named COPYING that should have accompanied this file.
+#
+# THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+BASE=/tmp
+UPSTREAM_REPO=https://svn.code.sf.net/p/bzflag/code
+UPSTREAM_UUID=08b3d480-bf2c-0410-a26f-811ee3361c24
+SVN_REPO=file:///scratch/bzflag/bzflag.svn	# $UPSTREAM_REPO will be slower
+
+svn log --xml $SVN_REPO | sed -e 's=\.[0-9][0-9][0-9][0-9][0-9][0-9]Z</date>=Z</date>=' -e 's=\(.\)</msg>=\1\n</msg>=' -e 's/>JeffM2501</>jeffm2501</' -e 's/  *$//' > $BASE/log.svn
+
+for repo in bzflag-bzflag bzflag-archive bzflag-tools bzflag-web bzworkbench ; do
+	cd $BASE/svn2git.$repo
+	git log --date=iso --grep=git-svn-id:
+done | `dirname $0`/git_log_to_xml.pl > $BASE/log.git
